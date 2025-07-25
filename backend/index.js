04c51f987960751e1dbe3bf5e8529190979a9767
Kirill Kochetkov <icejayms@gmail.com>
2025-07-25 23:19:02 +0300

    fixed proxy

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1); // доверие первому прокси (nginx)
 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'kboard_super_secret_key',
